672dcb08fdf4f2b0d8552c1cf6a6258a7c5bc07f
百里 <sgfoot2020@gmail.com>
2019-05-10 18:49:19 +0800

    grpc学习实例

---
+++ gprc/readme.md
@@ -0,0 +1,5 @@
+#   Grpc
+
+
+## 资料
+- https://grpc.io/docs/quickstart/go/
---
+++ gprc/server/app/services/chat.go
@@ -0,0 +1,28 @@
+package services
+
+import (
+	"context"
+	"github.com/yezihack/studyGo/gprc/server/proto"
+)
+
+//定义接口
+type ChatServer interface {
+	Send(ctx context.Context, req chat.SendRequest) (reply chat.SendReply, err error)
+	Receive(ctx context.Context, req chat.ReceiveRequest) (reply chat.ReceiveReply, err error)
+}
+
+//定义一个接口体,实现接口
+type ChatServe struct {
+}
+//实现接口里的方法: send
+func (cs *ChatServe) Send(ctx context.Context, req chat.SendRequest) (reply chat.SendReply, err error){
+	return
+}
+//实现接口里的方法: receive
+func (cs *ChatServe) Receive(ctx context.Context, req chat.ReceiveRequest) (reply chat.ReceiveReply, err error) {
+	return
+}
+//对外提供接口调用的方法
+func GetChatServe() ChatServer {
+	return &ChatServe{}
+}
