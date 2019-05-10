b9b39de4c968bb15bc1fbe628104bde07e0fa176
百里 <sgfoot2020@gmail.com>
2019-05-10 19:14:05 +0800

    add grpc

--- gprc/server/app/main.go
+++ gprc/server/app/main.go
@@ -1,1 +1,28 @@
-package app
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"github.com/yezihack/studyGo/gprc/server/app/services"
+	chat "github.com/yezihack/studyGo/gprc/server/proto"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	port = ":8009"
+)
+
+func main() {
+	log.SetOutput(os.Stdout)
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	s := grpc.NewServer()
+	chat.RegisterChatServiceServer(s, services.GetChatServe())
+	log.Println("server start...")
+	s.Serve(lis)
+}
--- gprc/server/app/services/chat.go
+++ gprc/server/app/services/chat.go
@@ -8,8 +8,8 @@ import (
 
 //定义接口
 type ChatServer interface {
-	Send(ctx context.Context, req chat.SendRequest) (reply chat.SendReply, err error)
-	Receive(ctx context.Context, req chat.ReceiveRequest) (reply chat.ReceiveReply, err error)
+	Send(ctx context.Context, req *chat.SendRequest) (reply *chat.SendReply, err error)
+	Receive(ctx context.Context, req *chat.ReceiveRequest) (reply *chat.ReceiveReply, err error)
 }
 
 //定义一个接口体,实现接口
@@ -17,12 +17,15 @@ type ChatServe struct {
 }
 
 //实现接口里的方法: send
-func (cs *ChatServe) Send(ctx context.Context, req chat.SendRequest) (reply chat.SendReply, err error) {
+func (cs *ChatServe) Send(ctx context.Context, req *chat.SendRequest) (reply *chat.SendReply, err error) {
+	reply.Status = 200
+	reply.Msg = "发送成功, 你发送的消息, content: " + req.Content
 	return
 }
 
 //实现接口里的方法: receive
-func (cs *ChatServe) Receive(ctx context.Context, req chat.ReceiveRequest) (reply chat.ReceiveReply, err error) {
+func (cs *ChatServe) Receive(ctx context.Context, req *chat.ReceiveRequest) (reply *chat.ReceiveReply, err error) {
+
 	return
 }
 
